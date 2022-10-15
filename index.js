8c1b1e6e9c040057d5ea12277a26dabe40a3f873
Robert Bittick <robertbittick@gmail.com>
2022-10-14 21:59:54 -0700

    commit tree 5c921b0c71d5dbbfdfce71c067e1092ddcf960d9

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name,email) {
+    const configureObject = {
+        method: 'POST',
+        headers:  {
+            'Content-Type': 'application/json',
+            'Accept':'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        }),  
+    } 
+    return fetch('http://localhost:3000/users', configureObject)
+        .then(response => response.json())
+        .then(newUser => document.body.innerHTML = newUser['id'])
+        .catch(error => document.body.innerHTML = error.message) 
+}
+submitData()
